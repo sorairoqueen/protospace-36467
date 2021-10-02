afb4d97faccb3e9c752f974a7645656c2de47baf
sorairoqueen <48656738+sorairoqueen@users.noreply.github.com>
2021-10-02 20:36:54 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.create(comment_params)
+    @prototype = Prototype.find(params[:prototype_id])
+    if @comment.save
+      @comment.save
+      redirect_to prototype_path(@comment.prototype.id)
+    else
+      render "prototypes/_prototype", locals: {prototype: @prototype}
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+end
---
+++ app/models/prototype.rb
@@ -0,0 +1,8 @@
+class Prototype < ApplicationRecord
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
+  belongs_to :user
+  has_one_attached :image
+  has_many :comments, dependent: :destroy
+end
